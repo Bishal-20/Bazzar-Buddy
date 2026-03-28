fb2d6777f17c5779468841bbe9e0f4d9e3ed1739
Bishal-20 <161943871+Bishal-20@users.noreply.github.com>
2026-03-28 19:23:59 +0530

    Refactor CORS setup in app.js
    Refactor CORS configuration to use array syntax and add allowed methods and headers.

--- server/app.js
+++ server/app.js
@@ -6,22 +6,14 @@ const path = require("path");
 const cors = require('cors');
 require('dotenv').config();
 const authJwt = require('./helper/jwt.js');
-const allowedOrigins = [
-  "https://bazzar-buddy-client.onrender.com",
-  "https://bazzar-buddy-admin.onrender.com",
-  "http://localhost:3000"
-];
-
-
 app.use(cors({
-  origin: function(origin, callback) {
-    if (!origin) return callback(null, true); // allow non-browser requests
-    if (allowedOrigins.indexOf(origin) === -1) {
-      return callback(new Error("CORS policy blocked this origin"), false);
-    }
-    return callback(null, true);
-  },
-  credentials: true 
+  origin: [
+    "https://bazzar-buddy-client.onrender.com",
+    "https://bazzar-buddy-admin.onrender.com",
+    "http://localhost:3000"
+  ],
+  methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
+  allowedHeaders: ["Content-Type"]
 }));
 
 // Body parser
